9370f71f83d2fbc5ca19b41d286b1fb11b0dbe0d
liyicky <liyicky@gmail.com>
2016-09-20 15:55:30 +0900

    Userdetails View

---
+++ PragmaticTweets/UserDetailViewController.swift
@@ -0,0 +1,12 @@
+//
+//  UserDetailsViewController.swift
+//  PragmaticTweets
+//
+//  Created by Jason Cheladyn on 9/20/16.
+//  Copyright © 2016 Liyicky. All rights reserved.
+//
+
+import UIKit
+
+class UserDetailViewController: 
+
